3b3cc0b770fa68295a9294bfdc59eec7fa2a4d94
CarameloMan <rhuan.gabriel@academico.ufpb.br>
2022-02-13 12:10:02 -0300

    Lista Vetor

---
+++ ListaVetor/ModuloVetor.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int addF (int *vetor, int cv, int valor);
+
+int addI (int *vetor, int cv, int valor);
+
+void listar (int *vetor, int cv);
+
+int limpar ();
+
+int removerF (int cv);
+
+int removerI (int *vetor, int cv);
